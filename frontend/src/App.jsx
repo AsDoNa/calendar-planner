f84a8ac29b27aa027652b104533d10b61f09dac6
Asher Nagler <AsDoNa@icloud.com>
2025-06-19 16:47:34 +0100

    Initialised basic calendar UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Calendar1 from "./components/Calendar1";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar
+        content={
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/calendar1" element={<Calendar1 />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        }
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
